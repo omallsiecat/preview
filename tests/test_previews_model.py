b72c9b48460b6cdbe2e7c11e55e4c0445a0ee9c9
David Hariri <david.hariri@gmail.com>
2017-01-27 18:18:02 -0500

    fixes unit tests

--- tests/test_previews_model.py
+++ tests/test_previews_model.py
@@ -23,13 +23,13 @@ class PreviewUnitTests(unittest.TestCase):
             },
             {
                 "url": "http://productioncommunity.publicmobile.ca/t5/Announcements/Update-on-support/m-p/114863",
-                "desc": "Hello Community, I want to give you an update on where we are at right now, since the last time I did so was last week. First, I want to start off by",
+                "desc": "Hello Community, I want to give you an update on where we are at right now, since the last time I did so was last week. First, I want...",
                 "icon": "http://rsxze77497.i.lithium.com/html/assets/PublicMobile_Favicon.ico?A019DE33B730A1F6B2A0A91F5E58D31F",
                 "title": "Update on support"
             },
             {
                 "url": "https://letterboxd.com/",
-                "desc": "Letterboxd is a social network for sharing your taste in film. Use it as a diary to record your opinion about films as you watch them, or just to keep track of films you’ve seen in the past. Rate, review and tag films as you add them. Find and follow your friends to see what they’re enjoying. Keep a watchlist of films you’d like to see, and create lists/collections on any topic.",
+                "desc": "Letterboxd is a social network for sharing your taste in film. Use it as a diary to record your opinion about films as you watch them, or just to...",
                 "icon": "https://s1.ltrbxd.com/static/img/icons/196.d0437325.png",
                 "title": "Letterboxd • Your life in film"
             },
