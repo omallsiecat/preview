d86bf72886eb44726a90c646a2959512b6a2b9d8
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2020-05-12 15:39:26 -0400

    Revert "Refactoring"
    This reverts commit b859cd7149a63f3ca4cafc77898de66a87d78ea5.

--- previews/model.py
+++ previews/model.py
@@ -34,18 +34,6 @@ class Preview(object):
         self.icon = icon
         self.image = image
 
-    def __get_description(self, htmlSoup):
-        desc_elems = htmlSoup.findAll("meta", attrs={"name": re.compile(r"Desc", re.I)})
-
-        if len(desc_elems) == 0:
-            return ""
-
-        for meta_elem in desc_elems:
-            if meta_elem.attrs["content"]:
-                return meta_elem.attrs["content"]
-            else:
-                return ""
-
     def fetch(self, timeout=1):
         """
         Fetches the url, parses the title, desc and icon
@@ -85,7 +73,13 @@ class Preview(object):
         self.title = titles[0].strip()
 
         # Get the desc from whatever we can find
-        self.desc = self.__get_description(soup)
+        desc_elems = soup.findAll("meta", attrs={"name": re.compile(r"Desc", re.I)})
+
+        for meta_elem in desc_elems:
+            if meta_elem.attrs["content"]:
+                self.desc = meta_elem.attrs["content"]
+            else:
+                self.desc = ""
 
         if len(self.desc.split()) > 30:
             self.desc = " ".join(self.desc.split()[0:29]).strip()
